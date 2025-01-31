692a5cfdb6d52ec477468581739850ea977d77ba
Vinicio D. Brejinski <viniciosdb@gmail.com>
2025-01-31 18:38:41 -0300

    first commit

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,9 +3,11 @@ import { autehnticate } from './authenticate'
 import { registerUser } from './register'
 import { profile } from './profile'
 import { FastifyInstance } from 'fastify'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', registerUser)
   app.post('/sessions', autehnticate)
+  app.patch('/token/refresh', refresh)
   app.get('/me', { onRequest: [verifyJWT] }, profile)
 }
